d98212c0f3c6934c90d7c46ad7b5bd5b80d64304
wesg52 <rwg97@cornell.edu>
2019-05-11 18:52:58 -0400

    Confidence values

--- eval.py
+++ eval.py
@@ -3,10 +3,11 @@ from torchtext import data, datasets
 from utils import get_arg_tokenizer, get_arg_tokenizer_eval, whitespace_tokenizer, get_pretrained_embeddings
 from transformer.my_iterator import MyIterator, rebatch
 from transformer.flow import make_model, batch_size_fn, run_epoch
-from transformer.greedy import greedy_decode
+from transformer.greedy import greedy_decode_eval
 from torch.autograd import Variable
 from pytorch_pretrained_bert import OpenAIGPTTokenizer, OpenAIGPTModel
 import time
+import numpy as np
 
 BATCH_SIZE = 2048
 MAX_OUTPUTS = 20
@@ -17,6 +18,7 @@ TGT_VOCABULARY = './models/noie_full_6heads_2048fftrg_vocab.pt'
 TRAINED_MODEL = './models/noie_full_1200ff_6heads_r5_epoch0.pt'
 TESTING_FILE = './all.txt'
 OUTPUT_FILE = './output_2.txt'
+MODE = 'mean' # 'mean' 'min' or a number between 0 and 100 representing the percentile (5-10 probably a good value)
 
 def get_testset(train_path):
 
@@ -99,14 +101,21 @@ if __name__ == '__main__':
         src = torch.LongTensor([[vocab_src.stoi[w] for w in sent]])
         src = Variable(src)
         src_mask = (src != vocab_src.stoi['<blank>']).unsqueeze(-2)
-        out = greedy_decode(model, src, src_mask, max_len=60, start_symbol=vocab_tgt.stoi["<s>"])
+        out, probs = greedy_decode_eval(model, src, src_mask, max_len=60, start_symbol=vocab_tgt.stoi["<s>"], end_symbol=vocab_tgt.stoi["</s>"])
+        if MODE == 'mean':
+            prob = np.mean(np.exp(probs.flatten()[1:].detach().numpy()))
+        elif MODE == 'min':
+            prob = np.min(np.exp(probs.flatten()[1:].detach().numpy()))
+        else:
+            prob = np.percentile(np.exp(probs.flatten()[1:].detach().numpy()), MODE)
+
         result = '<s> '
         for i in range(1, out.size(1)):
             sym = vocab_tgt.itos[out[0, i]]
             if sym == "</s>": 
                 break
             result += sym + " "
-        out_file.write(result + '\n')
+        out_file.write(result + '\t' + str(prob) + '\n')
     out_file.close()
     print('Extraction complete')
     end = time.time()
--- transformer/greedy.py
+++ transformer/greedy.py
@@ -15,4 +15,21 @@ def greedy_decode(model, src, src_mask, max_len, start_symbol):
         _, next_word = torch.max(prob, dim=1)
         next_word = next_word.data[0]
         ys = torch.cat([ys, torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
-    return ys
+    return ys, probs
+
+
+def greedy_decode_eval(model, src, src_mask, max_len, start_symbol, end_symbol):
+    memory = model.encode(src, src_mask)
+    ys = torch.ones(1, 1).fill_(start_symbol).type_as(src.data)
+    probs = torch.ones(1,1)
+    for i in range(max_len - 1):
+        out = model.decode(memory, src_mask, Variable(ys), Variable(subsequent_mask(ys.size(1)).type_as(src.data)))
+        prob = model.generator(out[:, -1])
+        word_prob, next_word = torch.max(prob, dim=1)
+        next_word = next_word.data[0]
+        word_prob = word_prob.data[0]
+        if next_word == end_symbol:
+            return ys, probs
+        ys = torch.cat([ys, torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
+        probs = torch.cat([probs, torch.ones(1,1).fill_(word_prob)], dim=1)
+    return ys, probs
